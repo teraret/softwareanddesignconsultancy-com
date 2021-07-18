16cd2a5c056317489a0d99342b8813f36c7c0134
harishbabubk <babuamuda@gmail.com>
2021-07-19 02:05:45 +0530

    fix

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from '@material-ui/core/styles';
+
+// Create a theme instance.
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#f6612c',
+        },
+        secondary: {
+            main: '#8a3d00',
+        },
+    },
+});
+
+export default theme;
